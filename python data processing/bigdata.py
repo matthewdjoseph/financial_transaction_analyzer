8d1e03117f3048e210593f028043a5780eb965f7
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-04-22 16:09:16 -0400

    WIP: saving dataframe to mysql

--- python data processing/bigdata.py
+++ python data processing/bigdata.py
@@ -1,25 +1,14 @@
 import pandas as pd 
-import mysql.connector
-from mysql.connector import Error
+from sqlalchemy import create_engine
 from pandas.io import sql
 
 df = pd.read_csv (r'C:\Users\mdjos\workspaces\financial_transaction_analyzer\dataset\PS_20174392719_1491204439457_log.csv')
 
 print (df)
 
-connection = mysql.connector.connect(host='localhost',
-                                     database='transactiondb',
-                                     user=[user],
-                                     password=[password],
-                                     port='3308')
-        
-if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+engine = create_engine("mysql://[user]:[password]@localhost:3308/transactiondb")
+
+connection = engine.connect()
         
 sql.to_sql(df, con=connection, name='transactions', if_exists='replace')
         
