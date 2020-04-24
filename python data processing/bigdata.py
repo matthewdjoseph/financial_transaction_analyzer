8c41fd145a3a7a868e9224cd028b702abf268d15
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-04-23 20:12:24 -0400

    broke dataframe up to handle large datasets

--- python data processing/bigdata.py
+++ python data processing/bigdata.py
@@ -1,15 +1,38 @@
 import pandas as pd 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from pandas.io import sql
 
-df = pd.read_csv (r'C:\Users\mdjos\workspaces\financial_transaction_analyzer\dataset\test_data.csv')
+df = pd.read_csv (r'C:\Users\mdjos\workspaces\financial_transaction_analyzer\dataset\PS_20174392719_1491204439457_log.csv')
 
-print (df)
+print ("File read...")
+
+size = 500000
+
+list_of_dfs = [df.loc[i:i+size-1,:] for i in range(0, len(df),size)]
+
+print ("File split...")
 
 engine = create_engine("mysql://[user]:[password]@localhost:3308/transactiondb")
 
 connection = engine.connect()
+
+try:
+    connection.execute("TRUNCATE TABLE transactions")
+    
+    print ("Table cleared...")
+    
+    for i in list_of_dfs:
+        sql.to_sql(i, 
+                   con=connection, 
+                   name='transactions', 
+                   schema='transactiondb', 
+                   if_exists='append',
+                   method=None,
+                   chunksize=10000)
+        print ("Finished a group...")
+    
+except Exception as e:
+    print (e)
         
-sql.to_sql(df, con=connection, name='transactions', schema='transactiondb', if_exists='replace')
-        
+print ('done....')
 connection.close()
