9d6c598b71398a4a2aa9e60e5485d738b69f944b
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-05-02 12:03:44 -0400

    started data service

---
+++ dataProcessor/src/main/java/com/financialTransactionAnalyzer/dataProcessor/server/SpringBootServer.java
@@ -0,0 +1,13 @@
+package com.financialTransactionAnalyzer.dataProcessor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServer.class, args);
+	}
+
+}
